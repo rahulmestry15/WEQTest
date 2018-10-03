8ba8012be782842892a41bb5a29829d0e55de919
rahulshantarammestry <rahulm@weqcorporation.com>
2018-10-03 09:49:48 +0530

    Initial commit

---
+++ app/src/main/java/com/technologies/android/weq/weqtest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.technologies.android.weq.weqtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.technologies.android.weq.myutils.MyMaths;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int a=MyMaths.add(1,2);
+        Toast toast = Toast.makeText(getApplicationContext(), "sum "+a, Toast.LENGTH_LONG);
+        toast.show();
+    }
+}
